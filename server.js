a5a9674b44957498b4dc481674e7002a4d026386
Sujin Lee <angelasujin@gmail.com>
2016-01-19 14:36:53 -0500

    change server port, express static directory

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ import config from './webpack.config';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'static')));
 
-http.createServer(app).listen(1337, '127.0.0.1');
+http.createServer(app).listen(1337, '0.0.0.0');
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://0.0.0.0:1337/');
 
 app.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname,'index.html'),function(){
