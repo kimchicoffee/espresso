2c1252b650f5a086b82603b62e51d5e859382f15
Sujin Lee <pasdesujin@gmail.com>
2016-01-23 13:14:33 -0500

    change dev-serve from webpack-dev-server to webpack-dev-middleware

--- server.js
+++ server.js
@@ -3,47 +3,43 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import mongoose from 'mongoose';
-
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
-import config from './webpack.config';
-
 import handlebars from 'handlebars';
 
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config';
+
+
 //Connet to database
-mongoose.connect('mongodb://localhost/pasdesujin-dev');
-mongoose.connection.on('connected', function() {
-	console.log('mongoose is connected to mongodb://localhost/pasdesujin-dev');
-});
+//mongoose.connect('mongodb://localhost/pasdesujin-dev');
+//mongoose.connection.on('connected', function() {
+//	console.log('mongoose is connected to mongodb://localhost/pasdesujin-dev');
+//});
 
 let app = express();
+let port = 3000;
 
-app.use(express.static(path.join(__dirname, 'static')));
-
+//compile handlebars to html
 let rawhtml = fs.readFileSync(path.join(__dirname,'index.html'), 'utf8');
 let template = handlebars.compile(rawhtml);
 let output = template({
   title : "pasdesujin"
 });
 
-http.createServer(app).listen(1337, '0.0.0.0');
-
-console.log('Server running at http://0.0.0.0:1337/');
-
+// route
 app.get('/', function (req, res, next) {
 	res.send(output);
 });
 
+let compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(err);
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
   }
-
-  console.log('Listening at 0.0.0.0:3000');
-});
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,32 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+        devtool: 'cheap-module-eval-source-map',
 		entry: [
-		    'webpack-dev-server/client?http://0.0.0.0:3000', //0.0.0.0 allow access from same WiFi network
-    		'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+		    'webpack-hot-middleware/client',
 			'./index.js'
 		],
 		output: {
-			path: path.join(__dirname,'/build'),
+			path: path.join(__dirname,'build'),
 			filename: 'bundle.js',
 			publicPath: '/static/'
 		},
 		plugins: [
-    		new webpack.HotModuleReplacementPlugin()
+            new webpack.optimize.OccurenceOrderPlugin(),
+    		new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoErrorsPlugin()
   		],
     	module: {
     		loaders: [
-    		{
+    		  {
         		test: /\.js?$/,
         		loaders: [ 'react-hot', 'babel' ],
         		exclude: /node_modules/
-    		},
-    		{
+    		  },
+    		  {
     			test: /\.scss$/,
     			loaders: [ 'style', 'css', 'sass']
-    		}
+    		  }
     		]
     	}
 }
